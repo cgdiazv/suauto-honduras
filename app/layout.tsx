99031632540f33e79e99eb37d48bed9e8742a81e
cgdiazv <cgdiazv@gmail.com>
2026-06-13 23:00:34 -0500

    No zoom ios forms

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
@@ -8,6 +8,12 @@ import { ReactNode } from "react";
 import { LanguageProvider } from '@/context/LanguageContext'; // 🔑 Importar
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
 
 export const metadata: Metadata = {
   title: "Su Auto Honduras | Venta de Vehículos en San Pedro Sula",
